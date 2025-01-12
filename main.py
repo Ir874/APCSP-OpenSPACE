795e10c6ceb7563194716020df5ce01bc9b4aa9e
Ishaan <ishaanmrao29@gmail.com>
2025-01-12 14:02:10 -0500

    Added functions to retrieve data from today and yesterday

--- main.py
+++ main.py
@@ -2,11 +2,21 @@ import requests # allows HTTP requests
 from apikeys import NASA, GAPI # external API keys
 import google.generativeai as genai # ai compiler
 import os
+from datetime import date, timedelta
+
+
+# Today's date
+toda = date.today()
+yesterda = toda - timedelta(days=1)
+
+today = toda.strftime("%Y-%m-%d")
+# Yesterday's date
+yesterday = yesterda.strftime("%Y-%m-%d")
 
 
 # Google API config 
 MODEL = "gemini-1.5-flash"
-prompt = "Summarize the following text into 2 lines, prioritizing scientfic information, while keeping it simple enough for someone who is not an astronomer to understand. Make sure to include outstanding or visible details, explaining them also: "
+prompt = "Summarize the following text into 3 lines, prioritizing scientfic information, while keeping it simple enough for someone who is not an astronomer to understand. Make sure to include outstanding or visible details, explaining them also. Make sure to include details about how the photo was taken, if present: "
 
 def get_model(): # Initialize Google Gemini AI library to be callable
     genai.configure(api_key=GAPI)
@@ -39,12 +49,13 @@ def apod(data):
 
 f = open("data.txt", "w") # Opens data file in write-over mode
 
-def datagrab(api,api_key,param,prompt="",summarize=False): # --> Reusable function to grab data from API
+# Resusable data retrieval function, prioritized for NASA APOD api
+def datagrab(api,api_key,param,prompt="",summarize=False,date=today): # --> Reusable function to grab data from API
     with requests.Session() as session: # Initializes request session
 
         for i in apis:
 
-            with session.get(api, params={"api_key": api_key}) as response: # Opens api
+            with session.get(api, params={"api_key": api_key, "date":date}) as response: # Opens api
                 
                 response.raise_for_status() # Checks response ping
                 data = response.json()
@@ -61,9 +72,18 @@ def datagrab(api,api_key,param,prompt="",summarize=False): # --> Reusable functi
                 else:
                     print("Error: Runtime error")
 
-datagrab(apis['apod'],NASA,'url') # returns url of image
-datagrab(apis['apod'],NASA,'explanation')
-print("\n")
-datagrab(apis['apod'],NASA,'explanation',prompt,True) # returns summarized explanation of APOD image
+def todaydata(): # Grab today's data
+    datagrab(apis['apod'],NASA,'url') # returns url of image
+    print("\n")
+    datagrab(apis['apod'],NASA,'explanation') # returns regular explanation
+    print("\n")
+    datagrab(apis['apod'],NASA,'explanation',prompt,True) # returns summarized explanation of APOD image
+
+def yesterdata(): # Grab yesterday's data
+    datagrab(apis['apod'],NASA,'url',date=yesterday) # returns url of image
+    print("\n")
+    datagrab(apis['apod'],NASA,'explanation',date=yesterday) # returns regular explanation
+    print("\n")
+    datagrab(apis['apod'],NASA,'explanation',prompt,True,date=yesterday)
 
 f.close() # Closes file to prevent accidental writing
